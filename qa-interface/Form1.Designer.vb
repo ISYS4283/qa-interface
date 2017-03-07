6bd42baa442ab5c704e626f545bbd110921d78bd
Jeff Puckett <JPuckett@walton.uark.edu>
2017-03-07 18:57:09 +0000

    load questions into datagridview

--- qa-interface/Form1.Designer.vb
+++ qa-interface/Form1.Designer.vb
@@ -1,5 +1,5 @@
 ﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
-Partial Class Form1
+Partial Class Questions
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
@@ -22,9 +22,60 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.dgvQuestions = New System.Windows.Forms.DataGridView()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.LoadQuestionsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        CType(Me.dgvQuestions, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'dgvQuestions
+        '
+        Me.dgvQuestions.AllowUserToAddRows = False
+        Me.dgvQuestions.AllowUserToDeleteRows = False
+        Me.dgvQuestions.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill
+        Me.dgvQuestions.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvQuestions.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvQuestions.Location = New System.Drawing.Point(0, 24)
+        Me.dgvQuestions.Name = "dgvQuestions"
+        Me.dgvQuestions.ReadOnly = True
+        Me.dgvQuestions.Size = New System.Drawing.Size(992, 723)
+        Me.dgvQuestions.TabIndex = 0
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.LoadQuestionsToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(992, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'LoadQuestionsToolStripMenuItem
+        '
+        Me.LoadQuestionsToolStripMenuItem.Name = "LoadQuestionsToolStripMenuItem"
+        Me.LoadQuestionsToolStripMenuItem.Size = New System.Drawing.Size(101, 20)
+        Me.LoadQuestionsToolStripMenuItem.Text = "Load Questions"
+        '
+        'Questions
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(992, 747)
+        Me.Controls.Add(Me.dgvQuestions)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.Name = "Questions"
+        Me.Text = "Questions"
+        CType(Me.dgvQuestions, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents dgvQuestions As DataGridView
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents LoadQuestionsToolStripMenuItem As ToolStripMenuItem
 End Class
--- qa-interface/Form1.vb
+++ qa-interface/Form1.vb
@@ -1,3 +1,34 @@
-﻿Public Class Form1
+﻿Imports System.Data.Sql
+Imports System.Data.SqlClient
 
+Public Class Questions
+    Private Sub LoadQuestionsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles LoadQuestionsToolStripMenuItem.Click
+        Dim sqlcon As New SqlConnection With {.ConnectionString = "Server=essql1.walton.uark.edu;Database=isys4283jpucket;Trusted_Connection=yes;"}
+        Dim sqlcmd As SqlCommand
+        Dim sqlda As SqlDataAdapter
+        Dim sqldataset As DataSet
+
+        Dim query As String
+        query = "SELECT * FROM questions;"
+
+        Try
+            sqlcon.Open()
+            sqlcmd = New SqlCommand(query, sqlcon)
+            sqlda = New SqlDataAdapter(sqlcmd)
+            sqldataset = New DataSet
+            sqlda.Fill(sqldataset)
+            If sqldataset.Tables.Count > 0 Then
+                dgvQuestions.Refresh()
+                dgvQuestions.DataSource = sqldataset.Tables(0)
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+            Throw ex
+        Finally
+            If sqlcon.State = ConnectionState.Open Then
+                sqlcon.Close()
+            End If
+        End Try
+
+    End Sub
 End Class
